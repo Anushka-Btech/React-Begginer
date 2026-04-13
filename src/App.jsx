de70a762b6cf8e1bfd26430835fc9c7bf972a637
Anushka <anushkachhoker60@gmail.com>
2026-04-13 10:48:38 +0530

    Tailwind css:start

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 import {add,subtract} from './utility'
-import Sample from './Component/Sample'
-import HomePage from './Component/HomePage'
+import Sample from './component/Sample'
+import HomePage from './component/HomePage'
 
 
 function App() {
